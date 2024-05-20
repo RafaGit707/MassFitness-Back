d6ce8f19f5dd77fe56f0af4c63ac98fd524f6910
RafaGit707 <rgalcal303@g.educaand.es>
2024-05-20 13:05:06 +0200

    Constructores Servicios y Controladores

--- src/main/java/com/example/massfitness/entidades/Usuario.java
+++ src/main/java/com/example/massfitness/entidades/Usuario.java
@@ -18,7 +18,7 @@ public class Usuario {
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "datos_personales_id")
-    private DatosPersonales datosPersonales;
+    private DatosPersonales datos_personales;
 
     private int progresoFitness;
     private int cantidadPuntos;
@@ -32,12 +32,12 @@ public class Usuario {
     public Usuario() {
     }
 
-    public Usuario(int idUsuario, String nombre, String correo_electronico, String contrasena, DatosPersonales datosPersonales, int progresoFitness, int cantidadPuntos, List<Logro> logrosConseguidos) {
+    public Usuario(int idUsuario, String nombre, String correo_electronico, String contrasena, DatosPersonales datos_personales, int progresoFitness, int cantidadPuntos, List<Logro> logrosConseguidos) {
         this.idUsuario = idUsuario;
         this.nombre = nombre;
         this.correo_electronico = correo_electronico;
         this.contrasena = contrasena;
-        this.datosPersonales = datosPersonales;
+        this.datos_personales = datos_personales;
         this.progresoFitness = progresoFitness;
         this.cantidadPuntos = cantidadPuntos;
         this.logrosConseguidos = logrosConseguidos;
@@ -80,11 +80,11 @@ public class Usuario {
     }
 
     public DatosPersonales getDatosPersonales() {
-        return datosPersonales;
+        return datos_personales;
     }
 
-    public void setDatosPersonales(DatosPersonales datosPersonales) {
-        this.datosPersonales = datosPersonales;
+    public void setDatosPersonales(DatosPersonales datos_personales) {
+        this.datos_personales = datos_personales;
     }
 
     public int getProgresoFitness() {
